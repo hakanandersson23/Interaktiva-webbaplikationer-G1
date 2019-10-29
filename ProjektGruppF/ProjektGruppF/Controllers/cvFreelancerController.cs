a3708e4006ca23bb40718b6fcc8f5d823c273052
UserL <User@LAPTOP-B7HVL4K9>
2019-10-29 14:32:40 +0100

    skapade cvFreelancer controller

---
+++ ProjektGruppF/ProjektGruppF/Controllers/cvFreelancerController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjektGruppF.Models;
+
+namespace ProjektGruppF.Controllers
+{
+    public class cvFreelancerController : Controller
+    {
+        private ProjektGruppFEntities db = new ProjektGruppFEntities();
+
+        // GET: cvFreelancer
+        public ActionResult Index()
+        {
+            return View(db.cv.ToList());
+        }
+
+        // GET: cvFreelancer/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            cv cv = db.cv.Find(id);
+            if (cv == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cv);
+        }
+
+        // GET: cvFreelancer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: cvFreelancer/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "cv_id,birthday,nationality,drivers_license,registration_date")] cv cv)
+        {
+            if (ModelState.IsValid)
+            {
+                db.cv.Add(cv);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cv);
+        }
+
+        // GET: cvFreelancer/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            cv cv = db.cv.Find(id);
+            if (cv == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cv);
+        }
+
+        // POST: cvFreelancer/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "cv_id,birthday,nationality,drivers_license,registration_date")] cv cv)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cv).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cv);
+        }
+
+        // GET: cvFreelancer/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            cv cv = db.cv.Find(id);
+            if (cv == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cv);
+        }
+
+        // POST: cvFreelancer/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            cv cv = db.cv.Find(id);
+            db.cv.Remove(cv);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
