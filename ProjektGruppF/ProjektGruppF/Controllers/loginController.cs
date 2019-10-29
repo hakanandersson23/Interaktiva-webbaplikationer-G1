f3c95540439577f9514e6b9ea1d8e16cfe0c43cd
håkan <hakan.andersson23@gmail.com>
2019-10-29 13:30:15 +0100

    login in sida påbörjad

---
+++ ProjektGruppF/ProjektGruppF/Controllers/loginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjektGruppF.Controllers
+{
+    public class loginController : Controller
+    {
+        // GET: login
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult LoginView()
+        {
+
+            return View();
+        }
+    }
+}
