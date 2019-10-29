0398be43c952d33b49005c02fd89efc8b676a781
sabrinathao <sath1801@student.miun.se>
2019-10-29 14:26:24 +0100

    view model mapp och annat

---
+++ ProjektGruppF/ProjektGruppF/Controllers/CustomerLandingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjektGruppF.Controllers
+{
+    public class CustomerLandingController : Controller
+    {
+        // GET: CustomerLanding
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
