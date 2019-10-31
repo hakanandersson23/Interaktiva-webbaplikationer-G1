78d183aa2ec6647fb6c993270262cb6f076b6ca3
håkan <hakan.andersson23@gmail.com>
2019-10-31 13:57:52 +0100

    aa

---
+++ ProjektGruppF/ProjektGruppF/Models/Extended/freelancer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjektGruppF.Models
+{
+    public partial class freelancer
+    {
+
+
+    }
+
+    public class freelancerMetadatad
+    {
+        [Display(Name ="First Name")]
+        [Required(ErrorMessage ="Ooops, you forgot to enter you name!")]
+        public string firstname { get; set; }
+    }
+}
