4767cf3bb3128ff57c98c5f74a7a660c88f1213d
cfk23cfk <cfk23cfk@gmail.com>
2019-10-30 21:14:16 -0300

    uppdaterat mina sidor lite grann

---
+++ ProjektGruppF/ProjektGruppF/Models/savedFreelancersOperations.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProjektGruppF.ViewModels;
+using ProjektGruppF.Models;
+
+namespace ProjektGruppF.Models
+{
+    public class savedFreelancersOperations
+    {
+        ProjektGruppFEntities pg = new ProjektGruppFEntities();
+        public List<SavedFreelancersVM> AllFreelancers()
+        {
+            List<SavedFreelancersVM> savefreeMList = new List<SavedFreelancersVM>();
+
+            var savedFreelancersList = (from free in pg.freelancer
+                                        select new
+                                        {
+                                            free.firstname,
+                                            free.lastname,
+                                            free.adress,
+                                            free.phonenumber,
+                                            free.email,
+                                        }).ToList();
+            foreach (var item in savedFreelancersList)
+            {
+                SavedFreelancersVM objcvm = new SavedFreelancersVM();
+                objcvm.Firstname = item.firstname;
+                objcvm.Lastname = item.lastname;
+                objcvm.Adress = item.adress;
+                objcvm.Phonenumber = item.phonenumber;
+                objcvm.Email = item.email;
+                savefreeMList.Add(objcvm);
+            }
+            return savefreeMList;
+        }
+
+        public List<SavedFreelancersVM> AllSkills()
+        {
+            List<SavedFreelancersVM> allSkills = new List<SavedFreelancersVM>();
+
+            var savedFreelancersList = (from s in pg.skill
+                                        select new
+                                        {
+                                            s.name
+                                        }).ToList();
+            foreach (var item in savedFreelancersList)
+            {
+                SavedFreelancersVM objcvm = new SavedFreelancersVM();
+                objcvm.Skillname = item.name;
+                allSkills.Add(objcvm);
+            }
+            return allSkills;
+        }
+    }
+}
