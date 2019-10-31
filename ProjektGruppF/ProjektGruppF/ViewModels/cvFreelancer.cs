96e30c91111dfbe47ea68d8cfd830b39abad5ef6
UserL <User@LAPTOP-B7HVL4K9>
2019-10-31 08:46:12 +0100

    la till properties i viewmodel

--- ProjektGruppF/ProjektGruppF/ViewModels/cvFreelancer.cs
+++ ProjektGruppF/ProjektGruppF/ViewModels/cvFreelancer.cs
@@ -8,14 +8,33 @@ namespace ProjektGruppF.ViewModels
     public class cvFreelancer
     {
 
-        public virtual int cv_id { get; set; }
-        //public System.DateTime birthday { get; set; }
-        public DateTime birthday { get; set; }
-        public string nationality { get; set; }
-        public string drivers_license { get; set; }
-        //public System.DateTime registration_date { get; set; }
-        public DateTime registration_date { get; set; }
+        public virtual int Cv_id { get; set; }
+        public System.DateTime Birthday { get; set; }
+        //public DateTime Birthday { get; set; }
+        public string Nationality { get; set; }
+        public string Drivers_license { get; set; }
+
+        public System.DateTime Registration_date { get; set; }
+        //public DateTime Registration_date { get; set; }
 
         public string NameOfEducationalInstitution { get; set; }
+
+       
+        public string Education { get; set; } // ge problem annars
+
+        
+        public string Work_experience { get; set; }
+
+        
+        public string Expertise { get; set; }
+
+        
+        public string main_abilities { get; set; }
+
+       
+        public string Skill { get; set; }
+
+        
+        public string Language { get; set; }
     }
 }
