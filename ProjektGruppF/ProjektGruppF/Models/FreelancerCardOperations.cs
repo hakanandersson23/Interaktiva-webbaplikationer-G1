ad916ba208edbb91fa18227d75beba4ac4b303ce
sabrinathao <sath1801@student.miun.se>
2019-11-04 22:41:18 +0100

    print button fungerar

--- ProjektGruppF/ProjektGruppF/Models/FreelancerCardOperations.cs
+++ ProjektGruppF/ProjektGruppF/Models/FreelancerCardOperations.cs
@@ -9,22 +9,6 @@ namespace ProjektGruppF.Models
 {
     public class FreelancerCardOperations
     {
-        /*
-
-        public List<FreelanceCardVM> fcList = new List<FreelanceCardVM>
-                {
-                    new FreelanceCardVM{Name="Erik", Lastname="Oberg", Age=40 , Nationality="Swedish", Language="Swedish", Skill="Programming",Expertise="C#",Rank=5},
-                    new FreelanceCardVM{Name="Sabrina", Lastname="Thao", Age=21 , Nationality="Swedish", Language="Swedish", Skill="Programming",Expertise="html",Rank=2},
-                    new FreelanceCardVM{Name="Eva", Lastname="Evva", Age=42 , Nationality="English", Language="English", Skill="Programming",Expertise="C#",Rank=3},
-                    new FreelanceCardVM{Name="Joe", Lastname="Jooe", Age=23 , Nationality="Swedish", Language="Swedish", Skill="Programming",Expertise="C#",Rank=4},
-                    new FreelanceCardVM{Name="Ida", Lastname="Idda", Age=34 , Nationality="Finnish", Language="Finnish", Skill="Programming",Expertise="C#",Rank=1},
-                    new FreelanceCardVM{Name="Ola", Lastname="Olla", Age=51 , Nationality="Norwegian", Language="Norwegian", Skill="Programming",Expertise="C#",Rank=2}
-                };*/
-        /*
-        public List<FreelanceCardVM> GetCards()
-        {
-            return fcList;
-        }*/
         ProjektGruppFEntities1 pgfe = new ProjektGruppFEntities1();
         public List<SavedFreelancersVM> FreelancercardVMList() {
             ProjektGruppFEntities1 pgfe = new ProjektGruppFEntities1();
@@ -103,3 +87,4 @@ namespace ProjektGruppF.Models
 
     }
 }
+
