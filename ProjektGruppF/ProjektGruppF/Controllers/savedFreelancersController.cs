e4f92926f17a2b56a735ce8aa818254e5ea8d495
cfk23cfk <cfk23cfk@gmail.com>
2019-11-06 19:03:58 -0300

    filtrering........

--- ProjektGruppF/ProjektGruppF/Controllers/savedFreelancersController.cs
+++ ProjektGruppF/ProjektGruppF/Controllers/savedFreelancersController.cs
@@ -19,7 +19,6 @@ namespace ProjektGruppF.Controllers
             skills.Add("Web developing");
             skills.Add("Databases");
             skills.Add("Mobile applications developing");
-            skills.Add("Sesuna");
             SelectList skillsList = new SelectList(skills);
             ViewData["skillsList"] = skillsList;
             return View(sOP.AllFreelancers());
@@ -52,105 +51,6 @@ namespace ProjektGruppF.Controllers
                     break;
             }
             return Json(expertises);
-        }
-        public JsonResult GetRanks(string expertises)
-        {
-            List<int> ranks = new List<int>();
-            switch (expertises)
-            {
-                case "C#":
-                    ranks.Add(5);
-                    ranks.Add(4);
-                    ranks.Add(3);
-                    ranks.Add(2);
-                    ranks.Add(1);
-                    break;
-                case "Java":
-                    ranks.Add(5);
-                    ranks.Add(4);
-                    ranks.Add(3);
-                    ranks.Add(2);
-                    ranks.Add(1);
-                    break;
-                case "Python":
-                    ranks.Add(5);
-                    ranks.Add(4);
-                    ranks.Add(3);
-                    ranks.Add(2);
-                    ranks.Add(1);
-                    break;
-                case "HTML":
-                    ranks.Add(5);
-                    ranks.Add(4);
-                    ranks.Add(3);
-                    ranks.Add(2);
-                    ranks.Add(1);
-                    break;
-                case "CSS":
-                    ranks.Add(5);
-                    ranks.Add(4);
-                    ranks.Add(3);
-                    ranks.Add(2);
-                    ranks.Add(1);
-                    break;
-                case "Javascript":
-                    ranks.Add(5);
-                    ranks.Add(4);
-                    ranks.Add(3);
-                    ranks.Add(2);
-                    ranks.Add(1);
-                    break;
-                case "JQuery":
-                    ranks.Add(5);
-                    ranks.Add(4);
-                    ranks.Add(3);
-                    ranks.Add(2);
-                    ranks.Add(1);
-                    break;
-                case "PHP":
-                    ranks.Add(5);
-                    ranks.Add(4);
-                    ranks.Add(3);
-                    ranks.Add(2);
-                    ranks.Add(1);
-                    break;
-                case "Postgres":
-                    ranks.Add(5);
-                    ranks.Add(4);
-                    ranks.Add(3);
-                    ranks.Add(2);
-                    ranks.Add(1);
-                    break;
-                case "Oracle":
-                    ranks.Add(5);
-                    ranks.Add(4);
-                    ranks.Add(3);
-                    ranks.Add(2);
-                    ranks.Add(1);
-                    break;
-                case "MySQL":
-                    ranks.Add(5);
-                    ranks.Add(4);
-                    ranks.Add(3);
-                    ranks.Add(2);
-                    ranks.Add(1);
-                    break;
-                case "Android":
-                    ranks.Add(5);
-                    ranks.Add(4);
-                    ranks.Add(3);
-                    ranks.Add(2);
-                    ranks.Add(1);
-                    break;
-                case "IOS":
-                    ranks.Add(5);
-                    ranks.Add(4);
-                    ranks.Add(3);
-                    ranks.Add(2);
-                    ranks.Add(1);
-                    break;
-            }
-            return Json(ranks);
         }
     }
 }
