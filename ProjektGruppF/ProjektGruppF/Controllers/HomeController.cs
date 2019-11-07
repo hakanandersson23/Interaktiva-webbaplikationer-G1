75605597fcb196b285a165ed92c472b509cc971f
cfk23cfk <cfk23cfk@gmail.com>
2019-11-07 08:52:50 -0300

    lagt till hvudstartsida

--- ProjektGruppF/ProjektGruppF/Controllers/HomeController.cs
+++ ProjektGruppF/ProjektGruppF/Controllers/HomeController.cs
@@ -3,29 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProjektGruppF.Models;
+using ProjektGruppF.ViewModels;
 
 namespace ProjektGruppF.Controllers
 {
     public class HomeController : Controller
-    {//sabrinas testkommentar
+    {
         public ActionResult Index()
-        {
+        {           
             return View();  
         }
 
         public ActionResult About()
         {
-            //test kommentar Linda
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
-        // test kommentar Håkan
-        // test charlie
+
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
